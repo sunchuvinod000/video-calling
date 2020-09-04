0b894f95ccb3508fe9acd196a49af5f191ee0ecf
sunchuvinod000 <sunchuvinod000@gmail.com>
2020-09-04 16:21:17 +0530

    done

---
+++ public/script.js
@@ -0,0 +1,112 @@
+const socket = io('/'); 
+const videoGrid = document.getElementById('video-grid');
+
+var peer = new Peer(undefined, {
+    path: '/peerjs',
+    host: '/localhost',
+    port: '5000'
+});
+
+const myVideo = document.createElement('video');
+
+let myVideoStream;
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream =>{
+   myVideoStream = stream;
+   addVideoStream(myVideo , stream)
+
+   peer.on('call', call => {
+       
+       call.answer(stream);
+       
+       const new_video = document.createElement('video')
+  
+       call.on('stream', userVideoStream =>{
+           addVideoStream(new_video, userVideoStream)
+           
+       })
+   })
+   socket.on('user-connected', userId =>{
+   connectToNewUser(userId,stream);
+})
+ 
+
+let text = $('input');
+
+$('html').keydown((e) =>{
+    if(e.which==13 && text.val().length !==0){
+     
+        socket.emit('message', text.val())
+        text.val('')
+    }
+})
+socket.on('createMessage',message =>{
+    $('ul').append(`<li class="message"><b>User</b><br>${message}</li>`)
+    scrollBottom()
+})
+
+});
+
+
+
+peer.on('open', id => {
+
+    socket.emit('join-room', ROOM_ID, id);
+})
+
+
+
+
+const connectToNewUser = (userId, stream) =>{
+
+    const call = peer.call( userId , stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream =>{
+        console.log(userVideoStream);
+        addVideoStream(video, userVideoStream);
+    })
+}
+
+const addVideoStream = (myvideo,stream) =>{
+
+    myvideo.srcObject = stream;
+    myvideo.addEventListener('loadedmetadata', () =>{
+        myvideo.play();
+  })
+    videoGrid.append(myvideo);
+}
+const scrollBottom = () =>{
+
+    let d = $('.main_chat_window');
+    d.scrollTop(d.prop("scrollHeight"));
+}
+
+const muteUnmute = () =>{
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if(enabled){
+        myVideoStream.getAudioTracks()[0].enabled = false;
+        setUnmuteButton()
+    }
+    else{
+        setMuteButton()
+        myVideoStream.getAudioTracks()[0].enabled = true;
+
+    }
+   
+}
+const setMuteButton = () => {
+    const html =`
+    <i class="fa fa-microphone" aria-hidden="true"></i>
+    <span>Mute</span>
+    `
+    document.querySelector('.main_mute_button').innerHTML=html;
+}
+const setUnmuteButton = () =>{
+    const html = `
+    <i class="unmute fa fa-microphone-slash" aria-hidden="true"></i>
+    <span>UnMute</span>`
+    document.querySelector('.main_mute_button').innerHTML=html;
+
+}
